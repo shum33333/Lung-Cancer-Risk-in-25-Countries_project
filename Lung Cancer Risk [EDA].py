2259dc6786113b13e60d0b6775af5b4da577ad28
fangshan <shum33333@gmail.com>
2025-07-14 17:00:47 +0800

    更新20250714

--- Lung Cancer Risk [EDA].py
+++ Lung Cancer Risk [EDA].py
@@ -17,8 +17,8 @@ for dirname, _, filenames in os.walk('/kaggle/input'):
 
 #df = pd.read_csv('/kaggle/input/lung-cancer-risk-in-25-countries/lung_cancer_prediction_dataset.csv').iloc[:, 1:]
 
-df=pd.read_csv('D:/芳珊/202504_Python人工智慧與數據分析/Lung-Cancer-Risk-in-25-Countries_project/lung_cancer_prediction_dataset.csv')
-#df=pd.read_csv('D:\shan\power-bi_advanced-courses\Lung-Cancer-Risk-in-25-Countries_project/lung_cancer_prediction_dataset.csv')
+#df=pd.read_csv('D:/芳珊/202504_Python人工智慧與數據分析/Lung-Cancer-Risk-in-25-Countries_project/lung_cancer_prediction_dataset.csv')
+df=pd.read_csv('D:\shan\power-bi_advanced-courses\Lung-Cancer-Risk-in-25-Countries_project/lung_cancer_prediction_dataset.csv')
 df.head()
 print(df.head())
 
